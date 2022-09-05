55e83dcd8f2da467d29a563cb558e9c5dff8ddf3
Dmytro Gordon <g-d@ukr.net>
2022-09-05 16:39:26 +0300

    Fix boost.di installation

--- cmake/Conan/Boost.DI.cmake
+++ cmake/Conan/Boost.DI.cmake
@@ -1,5 +1,4 @@
 add_conan_package(di 1.2.0 CONFIG_OPTIONS di:with_extensions=True)
 find_package(di MODULE REQUIRED)
 include_directories(BEFORE SYSTEM ${di_INCLUDE_DIRS})
-add_library(Boost.DI INTERFACE)
-add_library(Boost::Boost.DI ALIAS Boost.DI)
+set(BOOST_DI_TARGET di::di)
