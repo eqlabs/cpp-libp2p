945c342a42f2e0de47d08a37aa293d15530292ed
Dmytro Gordon <g-d@ukr.net>
2022-09-08 15:36:14 +0300

    Support boost >= 1.76

--- include/libp2p/outcome/outcome.hpp
+++ include/libp2p/outcome/outcome.hpp
@@ -10,7 +10,17 @@
 #include <boost/outcome/success_failure.hpp>
 #include <boost/outcome/try.hpp>
 
+// Starting from commit d33e73cf2f643f73930518cb253bf80efbca70a4 in version 1.76
+// Boost Outcome doesn't add `auto&&` to a variable automatically. So we need to do it
+// manually for backward-compatitbility.
+#if BOOST_VERSION < 107600
 #define OUTCOME_TRY(...) BOOST_OUTCOME_TRY(__VA_ARGS__)
+#else
+#define OUTCOME_TRY_1(x) BOOST_OUTCOME_TRY(x)
+#define OUTCOME_TRY_2(x, y) BOOST_OUTCOME_TRY(auto&& x, y)
+#define _GET_OUTCOME_MACRO(_1, _2, NAME, ...) NAME
+#define OUTCOME_TRY(... ) _GET_OUTCOME_MACRO(__VA_ARGS__, OUTCOME_TRY_2, OUTCOME_TRY_1)(__VA_ARGS__)
+#endif
 
 #include <libp2p/outcome/outcome-register.hpp>
 
