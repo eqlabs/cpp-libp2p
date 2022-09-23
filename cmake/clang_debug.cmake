31b2102403d58c4ac1ff2dfdc6b1bfe97ce48ad4
Mariusz Pilarek <mariusz@eiger.co>
2022-09-23 12:59:17 +0200

    Removed unneeded cmake files, moved compiler cmakes to appropriate clang/gcc profiles.

--- cmake/clang_debug.cmake
+++ cmake/clang_debug.cmake
@@ -1,3 +1,5 @@
 set(CMAKE_BUILD_TYPE "Debug")
 set(CMAKE_CXX_COMPILER clang++ CACHE STRING "")
 set(CMAKE_C_COMPILER clang CACHE STRING "")
+
+include(${CMAKE_CURRENT_LIST_DIR}/toolchain/compiler/clang.cmake)
--- cmake/gcc_debug.cmake
+++ cmake/gcc_debug.cmake
@@ -1,3 +1,5 @@
 set(CMAKE_BUILD_TYPE "Debug")
 set(CMAKE_CXX_COMPILER g++ CACHE STRING "")
 set(CMAKE_C_COMPILER gcc CACHE STRING "")
+
+include(${CMAKE_CURRENT_LIST_DIR}/toolchain/compiler/gcc.cmake)
